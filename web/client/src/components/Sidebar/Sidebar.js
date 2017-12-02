9ca5050f85e2eebb02ecf47386550549c5938cb8
Kayke Mendes <kayke-dev@outlook.com>
2017-12-02 05:29:21 -0200

    menu

--- web/client/src/components/Sidebar/Sidebar.js
+++ web/client/src/components/Sidebar/Sidebar.js
@@ -7,7 +7,18 @@ class Sidebar extends React.Component {
 
   render() {
     return (
-      <div className={style.sidebar} />
+      <div className={style.sidebar}>
+          <div className="group-item">
+           <a href="#" className={`list-group-item ${style.sidebarLogo}`}>
+               <img src="/images/logo.png"/></a>
+           <a href="#" className={`list-group-item ${style.sidebarList}`}>
+             <i class=" icon-pencil icon-2x"></i><p>Chamado</p></a>
+           <a href="#" className={`list-group-item ${style.sidebarList}`}>
+             <p>Despacho</p></a>
+           <a href="#" className={`list-group-item ${style.sidebarList}`}>
+             <p>Equipe Médica</p></a>
+            </div>          
+      </div>
     )
   }
 }
